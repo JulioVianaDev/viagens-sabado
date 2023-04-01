4ed7c7e9d43a0b6080f6e06affa988ee5e734715
JulioVianaDev <JulioVianaDev@gmail.com>
2023-04-01 09:28:03 -0300

    seed criado

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Travel.create(
+  nome: Faker::Travel::Airport.name(size: 'large', region: 'united_states') ,
+  desc: Faker::Food.description,
+  data: Faker::Date.between_except(from: 3.years.ago, to: 10.years.from_now, excepted: Date.today),
+  price: Faker::Number.decimal(l_digits: 2),
+)
+
+puts Travel.last
