c909277baef79a0c9b177def0d913cfb0ad9db5b
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-06 10:27:57 -0300

    trocando temas

--- styles.js
+++ styles.js
@@ -1,9 +1,17 @@
 const body = document.getElementById("body");
-function darkTheme(){
-  body.classList.add("body-dark");
-  body.classList.remove("body-light")
-}
-function lightTheme(){
-  body.classList.add("body-light");
-  body.classList.remove("body-dark")
-}
+  const icons = document.getElementById("temasOptions");
+
+  function toggleTheme() {
+    const moonIcon = '<img id="moon" src="./images/moon.png" onclick="toggleTheme()"/>';
+    const sunIcon = '<img id="sun" src="./images/sun.png" onclick="toggleTheme()"/>';
+
+    if (body.classList.contains("body-dark")) {
+      body.classList.remove("body-dark");
+      body.classList.add("body-light");
+      icons.innerHTML = moonIcon;
+    } else {
+      body.classList.remove("body-light");
+      body.classList.add("body-dark");
+      icons.innerHTML = sunIcon;
+    }
+  }
