4d84998fc5a73454a18520c55f6fca0c8e4f8331
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-13 09:38:19 -0300

    animação de desaparecer

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,19 +1,29 @@
+import { FormatarData } from '../fn-helpers/Data';
 import './Card.css';
 import {BsTrash2Fill} from 'react-icons/bs'
+import { useState } from 'react';
 function Card(props) {
   const {id,data,desc,price} = props
+  const [isDelete,setIsDelete] = useState(false);
+
+  const deleteMode=()=>{
+    setIsDelete(true);
+    setTimeout(()=>{
+      props.deleteTravel(id)
+    },700)
+  }
   return (
     <>
-      <div className='card'>
+      <div className={isDelete? "card disappear": "card"}>
         <h1>{props.nome}</h1>
         <div className="conteudoCard">
           <p>{desc}</p>
-          <p>{data}</p>
+          <p>{FormatarData(data)}</p>
           <p>{price}</p>
         </div>
         <div className="bottom">
           <div className="botoes">
-            <div onClick={()=>props.deleteTravel(id)}>
+            <div onClick={()=>deleteMode()}>
               <BsTrash2Fill size={32}/>
             </div>
             <div className="icons">
